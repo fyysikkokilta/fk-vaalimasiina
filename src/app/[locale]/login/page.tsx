0e3e10421654ff45adf3683defc6b301ab427add
Aapo Laakkio <aapolaakkio@outlook.com>
2025-10-30 14:48:32 +0200

    refactor: make login page fully RSC

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -1,7 +1,12 @@
+/* eslint-disable no-restricted-imports */
+
+import Link from 'next/link'
 import { Locale } from 'next-intl'
 import { getTranslations, setRequestLocale } from 'next-intl/server'
+import { Suspense } from 'react'
 
-import Login from './client'
+import LoginError from '~/components/LoginError'
+import TitleWrapper from '~/components/TitleWrapper'
 
 export async function generateMetadata({
   params
@@ -25,5 +30,28 @@ export default async function LoginPage({
   const nextIntlLocale = locale as Locale
   setRequestLocale(nextIntlLocale)
 
-  return <Login />
+  const t = await getTranslations('Login')
+
+  return (
+    <TitleWrapper title={t('title')}>
+      <div className="flex justify-center">
+        <div className="w-full max-w-md">
+          <div className="space-y-4">
+            <div className="text-center">
+              <p className="text-fk-black mb-6">{t('signin_description')}</p>
+            </div>
+            <Suspense>
+              <LoginError />
+            </Suspense>
+            <Link
+              href="/api/auth/google"
+              className="bg-fk-yellow text-fk-black flex w-full cursor-pointer items-center justify-center gap-3 rounded-lg px-4 py-2 font-medium transition-colors hover:bg-amber-500 focus:ring-2 focus:ring-amber-500 focus:outline-none"
+            >
+              {t('signin_with_google')}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </TitleWrapper>
+  )
 }
